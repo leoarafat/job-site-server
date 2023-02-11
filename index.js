0004fbb525634d219b3d0039cc309b0e20219080
leoarafat10 <97153759+leoarafat10@users.noreply.github.com>
2023-02-11 21:16:06 +0600

    connect with momngoDB

--- index.js
+++ index.js
@@ -14,8 +14,7 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
 
-
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.q66zrl2.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.tjc9clz.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
